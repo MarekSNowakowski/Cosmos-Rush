507198805fe5054ee991c01cc350c09e83a156f2
mareq022 <mareq022@gmail.com>
2020-07-26 17:31:05 +0200

    dash rotation and squeezing

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,6 +17,7 @@ public class Player : Circle
     public float slowmo = 0.5f;
     public ParticleSystem playerPS;
     public PolygonCollider2D boundries;
+    private float angle;
 
     [Header("Components")]
     public Camera cam;
@@ -51,13 +52,16 @@ public class Player : Circle
             Vector3 currentPoint = cam.ScreenToWorldPoint(Input.mousePosition);
             currentPoint.z = 15;
             DrawLine(startPoint, currentPoint);
+            Vector2 lookAt = new Vector2(startPoint.x - currentPoint.x, startPoint.y - currentPoint.y);
+            Rotating(lookAt);
+            Squeeze(lookAt);
         }
         if(Input.GetMouseButtonUp(0))
         {
             Time.timeScale = 1;
             endPoint = cam.ScreenToWorldPoint(Input.mousePosition);
             endPoint.z = 15;
-
+            transform.localScale = Vector3.one;
             ballForce = new Vector2(Mathf.Clamp(startPoint.x - endPoint.x, minimumPower.x, maximumPower.x),
                 Mathf.Clamp(startPoint.y - endPoint.y, minimumPower.y, maximumPower.y));
             rb.velocity = Vector2.zero;
@@ -66,6 +70,32 @@ public class Player : Circle
             audioSource.Play();
             playerPS.Play();
         }
+    }
+
+    private void Rotating(Vector2 lookAt)
+    {
+        float atan = Mathf.Atan(lookAt.x / lookAt.y) * Mathf.Rad2Deg;
+        if (Mathf.Abs(lookAt.y) < Mathf.Epsilon) atan = 0;
+        if (lookAt.x > 0)
+        {
+            if (lookAt.y > 0) angle = -atan; //I
+            else if (lookAt.y < 0) angle = 180 - atan;        //IV
+            else angle = -90;
+        }
+        else
+        {
+            if (lookAt.y > 0) angle = -atan; //II
+            else if (lookAt.y < 0) angle = -180 - atan;        //III
+            else angle = 90;
+        }
+        transform.localEulerAngles = new Vector3(0, 0, angle);
+    }
+
+    private void Squeeze(Vector3 lookAt)
+    {
+        float length = Mathf.Sqrt(Mathf.Pow(lookAt.x,2f) + Mathf.Pow(lookAt.y, 2f));
+        float scaleY = 1 - length / 100;
+        transform.localScale = new Vector3(1, scaleY, 1);
     }
 
     private void endline()
